c74f01503524e9eb195b44af168a12fba01e52f7
Andrey Polischuk <andre.polischuk@gmail.com>
2016-08-15 16:13:16 +0300

    Clean HyperTerm config

--- .hyperterm.js
+++ .hyperterm.js
@@ -1,16 +1,15 @@
 module.exports = {
   config: {
     fontSize: 12,
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'Menlo, DejaVu Sans Mono, Lucida Console, monospace',
     padding: '10px',
-    windowSize: [ 1125, 825 ]
+    windowSize: [ 1225, 825 ]
   },
   plugins: [
     'hypercwd',
     'hyperterm-blink',
     'hyperterm-title',
-    'hyperterm-one-light',
-    'hyperterm-subpixel-antialiased'
+    'hyperterm-one-light'
   ],
   localPlugins: []
 };
