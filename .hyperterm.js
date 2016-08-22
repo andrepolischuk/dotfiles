53b8027d013ec69969f78280da62424005d8f3e5
Andrey Polischuk <andre.polischuk@gmail.com>
2016-08-22 17:59:59 +0300

    Update HyperTerm config

--- .hyperterm.js
+++ .hyperterm.js
@@ -2,13 +2,11 @@ module.exports = {
   config: {
     fontSize: 12,
     fontFamily: 'Menlo, DejaVu Sans Mono, Lucida Console, monospace',
-    padding: '10px',
-    windowSize: [ 1225, 825 ]
+    padding: '10px'
   },
   plugins: [
     'hypercwd',
     'hyperterm-blink',
-    'hyperterm-title',
     'hyperterm-one-light'
   ],
   localPlugins: []
