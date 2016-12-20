b4bdee48fd7ed5b0a82860f024313b51b0cd568f
Andrey Polischuk <andre.polischuk@gmail.com>
2016-12-20 19:00:17 +0300

    Update Hyper config

--- .hyper.js
+++ .hyper.js
@@ -2,12 +2,11 @@ module.exports = {
   config: {
     fontSize: 12,
     fontFamily: 'Menlo, DejaVu Sans Mono, Lucida Console, monospace',
-    padding: '10px'
+    padding: '10px',
+    cursorShape: 'BEAM',
   },
   plugins: [
     'hypercwd',
-    'hyperterm-blink',
-    'hyper-one-light'
+    'hyper-one-light',
   ],
-  localPlugins: []
 };
